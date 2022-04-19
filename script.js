33055fd8cea850969031330a5c751f2c7df36560
Randy King Jr <rkingjr@outlook.com>
2022-04-19 12:24:04 -0500

    add start screen after death

--- script.js
+++ script.js
@@ -75,6 +75,7 @@ function handleLose() {
     setDinoLose()
     setTimeout(() => {
         document.addEventListener("keydown", handleStart, { once: true})
+        startScreenElement.classList.remove("hide")
     }, 100)
 }
 
