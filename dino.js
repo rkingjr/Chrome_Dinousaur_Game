31869472430834f0fd9eb3544e4c58226d32a966
Randy King Jr <rkingjr@outlook.com>
2022-04-19 10:14:47 -0500

    add jumping animation and listener

--- dino.js
+++ dino.js
@@ -1,21 +1,28 @@
+import { incrementCustomProperty, getCustomProperty, setCustomProperty } from "./updateCustomProperty.js"
+
 const dinoElement = document.querySelector('[data-dino]')
 const jumpSpeed = 0.45
-const gravity = 0.011
+const gravity = 0.0015
 const dinoFrameCount = 2
 const frameTime = 100
 
 let isJumping
 let dinoFrame
 let currentFrameTime
+let yVelocity
 export function setupDino() {
     isJumping = false
     dinoFrame = 0
     currentFrameTime = 0
+    yVelocity = 0
+    setCustomProperty(dinoElement, "--bottom", 0)
+    document.removeEventListener("keydown", onJump)
+    document.addEventListener("keydown", onJump)
 }
 
 export function updateDino(delta, speedScale) {
     handleRun(delta, speedScale)
-    // handleJump()
+    handleJump(delta)
 }
 
 function handleRun(delta, speedScale) {
@@ -32,6 +39,22 @@ function handleRun(delta, speedScale) {
     currentFrameTime += delta * speedScale
 }
 
-// function handleJunp() {
+function handleJump(delta) {
+    if (!isJumping) return
 
-// }
+    incrementCustomProperty(dinoElement, "--bottom", yVelocity * delta)
+
+    if (getCustomProperty(dinoElement, "--bottom") <= 0) {
+        setCustomProperty(dinoElement, "--bottom", 0)
+        isJumping = false
+    }
+
+    yVelocity -= gravity * delta
+}
+
+function onJump(e) {
+    if (e.code !== "Space" || isJumping) return
+
+    yVelocity = jumpSpeed
+    isJumping = true
+}
